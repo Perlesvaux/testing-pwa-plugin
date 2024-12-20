70ad3a9386c9921971a1c28efe515b5b3dd5bb1e
Perlesvaux <paltryhumanity@gmail.com>
2024-12-20 17:27:42 -0600

    fix: reducer updated. Works fine with CRUD operations.

--- src/InputList.jsx
+++ src/InputList.jsx
@@ -1,8 +1,36 @@
-import {useState} from 'react'
+import {useReducer, useState} from 'react'
+
+const initialState = {
+  friends: { value:[], kind:"list" },
+}
+
+function reducer (state, action){
+
+  switch (action.type) {
+    case "addToList":
+      return { ...state, [action.field]:{...state[action.field], value:[...state[action.field].value, action.value]}}
+
+    case "removeFromList":
+      return { ...state, [action.field]:{...state[action.field], value:[...state[action.field].value].filter((elem, indx) => indx!==action.value)   }} 
+
+    case "updateEntryFromList":
+    {
+        const updated = [ ...state[action.field].value ]
+        updated[action.index][action.part] = action.value 
+        console.log(updated)
+        return {...state, [action.field]:{...state[action.field], value:updated }}
+    }
+
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+  
+}
 
 export default function InputList(){
 
-  const [state, setState] = useState({ friends:[]  }) // {name:"carlos", age:20}, {name:"alf", age:32}
+  const [state, dispatch] = useReducer(reducer, initialState)
+  //const [state, setState] = useState({ friends:[]  }) // {name:"carlos", age:20}, {name:"alf", age:32}
 
   const [ current, setCurrent ] = useState ( {
     name:'', age:''
@@ -25,28 +53,30 @@ export default function InputList(){
       <input type="number" name="age" value={current.age} onChange={getInput} />
     </label>
 
-    <div onClick={()=>{setState( { friends:[...state.friends, current] } ) ; setCurrent({ name:'', age:'' }) }}> ok </div>
+    <div onClick={()=>{dispatch( { type:"addToList", field:"friends", value: current} ) ; setCurrent({ name:'', age:'' }) }}> ok </div>
 
     
-    {state.friends.map((elem, i)=>
+    {state.friends.value.map((elem, i)=>
       <div key={i}>
         <input type="text"  value={elem.name} onChange={(e)=>
         {
-            const updated = [...state.friends]
-            updated[i].name = e.target.value
-            console.log(updated)
-            setState({friends:updated})
+            dispatch({ type:"updateEntryFromList", field: "friends", part:"name", index:i, value:e.target.value })
+            //const updated = [...state.friends]
+            //updated[i].name = e.target.value
+            //console.log(updated)
+            //setState({friends:updated})
         }
           } />
         <input type="number" value={elem.age} onChange={(e)=>
         {
-            const updated = [...state.friends]
-            updated[i].age = e.target.value
-            console.log(updated)
-            setState({friends:updated})
+            dispatch({ type:"updateEntryFromList", field: "friends", part:"age", index:i, value:e.target.value })
+            //const updated = [...state.friends]
+            //updated[i].age = e.target.value
+            //console.log(updated)
+            //setState({friends:updated})
         }
           } />
-        <div onClick={()=>{setState({ friends:state.friends.filter((elem, ii)=> ii!==i   )  })}}>X</div>
+        <div onClick={()=>{dispatch({type:"removeFromList", field:"friends", value:i})}}>X</div>
       </div>)}
 
 
